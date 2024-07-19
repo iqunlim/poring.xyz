0c049a267d6485e3529d1ca448a70bf51d140441
IQ <carpentedc@gmail.com>
2024-07-19 12:39:04 -0500

    Basic API GET with no CORS working

--- littleuploadapp-module/api.tf
+++ littleuploadapp-module/api.tf
@@ -20,6 +20,14 @@ resource "aws_api_gateway_method" "Sign-S3-GET" {
     resource_id = aws_api_gateway_resource.Sign-S3.id
     http_method = "GET"
     authorization = "NONE" # TODO: Add authorization to the API
+    depends_on = [
+        aws_api_gateway_resource.Sign-S3
+    ]
+    request_parameters = {
+        "method.request.querystring.fileName" = true
+        "method.request.querystring.fileType" = true
+        "method.request.querystring.t" = true
+    }
 }
 
 # This is the integration request (what you actually want to do with the API)
@@ -29,32 +37,25 @@ resource "aws_api_gateway_integration" "Sign-S3-Lambda" {
     rest_api_id = aws_api_gateway_rest_api.littleupload_api.id
     resource_id = aws_api_gateway_resource.Sign-S3.id
     http_method = "${aws_api_gateway_method.Sign-S3-GET.http_method}"
-    integration_http_method = "GET"
+    integration_http_method = "POST" # If you do not set this as POST, it will cause an auth error!
     type = "AWS" # TODO: Look in to AWS_PROXY integration request setting
     uri = aws_lambda_function.GetSignedS3.invoke_arn
-}
 
-resource "aws_api_gateway_method_response" "get_response_200" {
-    rest_api_id = aws_api_gateway_rest_api.littleupload_api.id
-    resource_id = aws_api_gateway_resource.Sign-S3.id
-    http_method = aws_api_gateway_method.Sign-S3-GET.http_method
-    status_code = "200"
-}
+    depends_on = [
+        aws_api_gateway_method.Sign-S3-GET
+    ]
 
-resource "aws_api_gateway_method_response" "options_response_200" {
-    rest_api_id = aws_api_gateway_rest_api.littleupload_api.id
-    resource_id = aws_api_gateway_resource.Sign-S3.id
-    http_method = aws_api_gateway_method.Sign-S3-OPTIONS.http_method
-    status_code = "200"
-    response_parameters = {
-        
+    request_templates = {
+        "application/json" = <<EOF
+{
+    "fileName":  "$input.params('fileName')",
+    "fileType":  "$input.params('fileType')", 
+    "t": "$input.params('t')"   
+}
+EOF
     }
 }
 
-
-# CORS from here below
-# This response is for the CORS policy
-# TODO: Make this only origin = var.domain after testing
 resource "aws_api_gateway_integration_response" "Sign-S3-Lambda" {
     rest_api_id = aws_api_gateway_rest_api.littleupload_api.id
     resource_id = aws_api_gateway_resource.Sign-S3.id
@@ -65,16 +66,28 @@ resource "aws_api_gateway_integration_response" "Sign-S3-Lambda" {
     }*/
     status_code = aws_api_gateway_method_response.get_response_200.status_code
     depends_on = [
+        aws_api_gateway_method.Sign-S3-GET,
         aws_api_gateway_integration.Sign-S3-Lambda
     ]
 }
-# This returns the CORS headers
-resource "aws_api_gateway_method" "Sign-S3-OPTIONS" {
+
+resource "aws_api_gateway_method_response" "get_response_200" {
+    rest_api_id = aws_api_gateway_rest_api.littleupload_api.id
+    resource_id = aws_api_gateway_resource.Sign-S3.id
+    http_method = aws_api_gateway_method.Sign-S3-GET.http_method
+    status_code = "200"
+    depends_on = [
+        aws_api_gateway_method.Sign-S3-GET
+    ]
+}
+
+# THE OPTIONS RESPONSE
+/*resource "aws_api_gateway_method" "Sign-S3-OPTIONS" {
     rest_api_id = aws_api_gateway_rest_api.littleupload_api.id
     resource_id = aws_api_gateway_resource.Sign-S3.id
     http_method = "OPTIONS"
     authorization = "NONE" # TODO: Add authorization to the API
-}
+}*/
 
 # This is the integration response with it for the CORS headers
 /*resource "aws_api_gateway_integration_response" "Sign-S3-Options" {
@@ -92,4 +105,17 @@ resource "aws_api_gateway_method" "Sign-S3-OPTIONS" {
     depends_on = [
         aws_api_gateway_integration.Sign-S3-Lambda
     ]
-}*/
+}
+
+resource "aws_api_gateway_method_response" "options_response_200" {
+    rest_api_id = aws_api_gateway_rest_api.littleupload_api.id
+    resource_id = aws_api_gateway_resource.Sign-S3.id
+    http_method = aws_api_gateway_method.Sign-S3-OPTIONS.http_method
+    status_code = "200"
+    response_parameters = {
+        
+    }
+}
+
+
+*/
