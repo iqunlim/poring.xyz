6c58eeb1538688c8cb62615415042cd59f4bf961
iqunlim <mrfoobar2000@gmail.com>
2025-04-09 15:56:49 -0500

    Add logo

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import FileUploadForm from "./components/FileUploadForm";
 import ClipboardButton from "./components/Clipboard";
 import { ApiError, putR2Object } from "./Api";
 import { Analytics } from "@vercel/analytics/react";
+import logo from "./assets/logo2.png";
 
 type fileData = {
   fileUrl: string;
@@ -17,17 +18,6 @@ function App() {
     const file = formData.get("image") as File;
     if (file) {
       try {
-        // const SignedData = await getSignedS3Url(file);
-        // if (SignedData && SignedData.url) {
-        //   putSignedS3Object(file, SignedData, SignedData.url).then(() => {
-        //     setFileUrls(prev => {
-        //       if (SignedData.imageUrl) {
-        //         return [SignedData.imageUrl, ...prev];
-        //       }
-        //       return prev
-        //     }
-        //     );
-        //   })
         const res = await putR2Object(file); // This feels hardcoded and should be more of an interface or injected
         if (res.error) {
           throw new ApiError(
@@ -66,14 +56,17 @@ function App() {
   };
 
   return (
-    <>
+    <main className="h-screen overflow-auto flex flex-col justify-center items-center">
+      <div className="self-end px-4 pt-2">
+        <img className="h-20 md:h-[100px]" src={logo} alt="poring.xyz" />
+      </div>
       <Analytics />
-      <div className="flex flex-col h-screen items-center justify-center m-auto max-w-[40%]">
+      <div className="flex flex-col h-full items-center justify-center m-auto max-w-[40%]">
         <div className="big-shadow bg-white flex flex-col gap-4 items-center justify-center max-w-full min-w-[300px] px-2 py-4 relative rounded-sm">
           <Mascot type="angeling" className="right-[50px] top-[-45px]" />
           <Mascot type="archangeling" className="left-[50px] top-[-45px]" />
           <FileUploadForm
-            className="relative flex justify-center align-center font-bold py-2 cursor-pointer text-center rounded-full shadow-md bg-red-200 hover:bg-red-300"
+            className="relative flex justify-center align-center font-bold py-2 cursor-pointer text-center rounded-full shadow-md bg-red-300 hover:bg-red-400"
             formActionFunction={action}
             allowpaste
           />
@@ -96,15 +89,15 @@ function App() {
             <ClipboardButton
               key={fileObj.fileUrl}
               text={fileObj.fileUrl}
-              className="bg-red-200 cursor-copy flex 
-              gap-1 hover:bg-red-300 items-center no-scrollbar 
+              className="bg-red-300 cursor-copy flex 
+              gap-1 hover:bg-red-400 items-center no-scrollbar 
               overflow-scroll p-1 relative rounded-md shadow-md 
               text-nowrap w-full"
             />
           ))}
         </div>
       </div>
-    </>
+    </main>
   );
 }
 
